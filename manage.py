39cad30f8ea86fd113567229c8d0d42f829d3b56
Yossef Rosenstein <yossef.rosenstein@gmail.com>
2015-12-09 01:13:33 -0500

    removed .pyc files from repo

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
-#from config import Config
+import os
+
 from app import app, db
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
-
 manager = Manager(app)
+
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
