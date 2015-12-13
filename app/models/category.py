59e2de46bf77971ee49e32a201dabbca44ba70d8
Yossef Rosenstein <yossef.rosenstein@gmail.com>
2015-12-12 19:45:49 -0500

    made categories unique and challenge titles too

--- app/models/category.py
+++ app/models/category.py
@@ -2,7 +2,7 @@ from app import db
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(50), unique=True)
 
     def __init__(self, name):
         self.name = name
--- app/models/challenge.py
+++ app/models/challenge.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Challenge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     points = db.Column(db.Integer)
-    title = db.Column(db.String(80))
+    title = db.Column(db.String(80), unique=True)
     pub_date = db.Column(db.DateTime)
     
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
