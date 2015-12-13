22ec1d07ea38e3168365995787a0d2c33c26315b
Yossef Rosenstein <yossef.rosenstein@gmail.com>
2015-12-12 20:58:42 -0500

    fixed challenge route bugs

--- app/challenges/controllers.py
+++ app/challenges/controllers.py
@@ -6,29 +6,38 @@ from app.models.challenge import Challenge
 from app.models.category import Category
 from app.models.score import Score
 
+def createSerializableList(challenges):
+    challengeList = []
+    for challenge in challenges:
+        challengeList.append({"id":challenge.id, "points":challenge.points, "title":challenge.title, "category":challenge.category.name})
+    return challengeList
+
+
 challenges = Blueprint('challenges', __name__)
+
 
 @challenges.route('/challenges', methods=['GET'])
 def get_list_of_all_challenges():
     challenges = Challenge.query.all()
-    response = json.jsonify(challenges=challenges, status=200)
+    
+    response = json.jsonify(challenges=createSerializableList(challenges), status=200)
     response.status_code = 200
     return response
 
 @challenges.route('/challenges/<string:challengeType>', methods=['GET'])
 def get_challenge_list_of_challenge_type(challengeType):
 
-    category = Category.query.filter_by(name=challengeType.lower())
+    category = Category.query.filter_by(name=challengeType).first()
     challenges = category.challenges.all()
 
-    response = json.jsonify(challenges=challenges, status=200)
+    response = json.jsonify(challenges=createSerializableList(challenges), status=200)
     response.status_code = 200
     return response
 
 @challenges.route('/challenges/<int:challengeid>', methods=['GET'])
 def get_challenge(challengeid):
-    challenge = Challenge.get(challengeid)
+    challenge = Challenge.query.get(challengeid)
 
-    response = json.jsonify(id=challenge.id, points=challenge.points, title=challenge.title, category=category.name, status=200)
+    response = json.jsonify(id=challenge.id, points=challenge.points, title=challenge.title, category=challenge.category.name, status=200)
     response.status_code = 200
     return response
--- app/models/challenge.py
+++ app/models/challenge.py
@@ -10,13 +10,10 @@ class Challenge(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     category = db.relationship('Category',
                                backref=db.backref('challenges', lazy='dynamic'))
-
+    
     def __init__(self, points, title, category):
         self.points = points
         self.title = title
         self.pub_date = datetime.utcnow()
         self.category = category
-
-    def __repr__(self):
-        return '<Challenge %r>' % self.name
         
